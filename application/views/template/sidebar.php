84cc87cb6625bedaf9d5643dd39ae6b10c0084e5
Unknown <rahmat.sayfuddin@binus.ac.id>
2018-05-30 10:43:41 +0700

    progress bar

--- application/views/template/sidebar.php
+++ application/views/template/sidebar.php
@@ -1,27 +1,29 @@
-   	<!-- START PAGE SIDEBAR -->
-   	<div class="page-sidebar">
-   		<!-- START X-NAVIGATION -->
-   		<ul class="x-navigation">
-   			<li class="xn-logo">
-   				<a href="/"><img src="<?php echo base_url() ?>/asset/img/logo_splash.png" class="img-responsive"></a>
-   				<a href="#" class="x-navigation-control"></a>
-   			</li>
-   			<li class="xn-profile">
-   				<div class="profile-mini">
-   					<img src="<?php echo base_url() ?>/asset/assets/images/users/avatar.jpg" alt="John Doe"/>
-   				</div>
-   				<div class="profile">
-   					<div class="profile-image">
-   						<img src="<?php echo base_url() ?>/asset/assets/images/users/avatar.jpg" alt="John Doe"/>
-   					</div>
-   					<div class="profile-data">
-   						<div class="profile-data-name">John Doe</div>
-   					</div>
-   				</div>                                                                        
-   			</li>
-   			<li class="<?php if(isset($all_files)){echo $all_files;}?>">
-   				<a href="<?php echo site_url() ?>/home"><span class="big-icon fa fa-folder"></span> <span class="xn-text">All File</span></a>                        
-   			</li>     
+<!-- START PAGE SIDEBAR -->
+<div class="page-sidebar">
+  <!-- START X-NAVIGATION -->
+  <ul class="x-navigation">
+   <li class="xn-logo">
+    <a href="/"><img src="<?php echo base_url() ?>/asset/img/logo_splash.png" class="img-responsive"></a>
+    <a href="#" class="x-navigation-control"></a>
+  </li>
+  <li class="xn-profile">
+    <div class="profile-mini">
+     <!-- <img src="<?php echo base_url() ?>/asset/assets/images/users/avatar.jpg" alt="John Doe"/> -->
+   </div>
+   <div class="profile">
+     <div class="profile-image">
+     <!--   <img src="<?php echo base_url() ?>/asset/assets/images/users/avatar.jpg" alt="John Doe"/>
+     -->
+     <span class="fa fa-user fa-5x"></span>
+   </div>
+   <div class="profile-data">
+    <div class="profile-data-name"><a href="<?php echo site_url() ?>/users" style="color: #222A38;">John Doe</a></div>
+  </div>
+</div>                                                                        
+</li>
+<li class="<?php if(isset($all_files)){echo $all_files;}?>">
+  <a href="<?php echo site_url() ?>/home"><span class="big-icon fa fa-folder"></span> <span class="xn-text">All File</span></a>                        
+</li>     
    			<!-- <li class="">
    				<a href="index.html">
    					<span class="big-icon fa fa-star">
@@ -55,7 +57,31 @@
    			</li>                
    		</ul>
    		<!-- END X-NAVIGATION -->
-   	</div>
-   	<!-- END PAGE SIDEBAR -->
+       <ul class="x-navigation">
+        <li class="xn-profile">
+         <div class="profile-mini">
+          <!-- <img src="<?php echo base_url() ?>/asset/assets/images/users/avatar.jpg" alt="John Doe"/> -->
+        </div>
+        <div class="profile">
+          <div class="profile-image">
+                     <!-- <img src="<?php echo base_url() ?>/asset/assets/images/users/avatar.jpg" alt="John Doe"/>
+                     -->
+
+                   </div>
+                   <div class="profile-data">
+                     <div class="profile-data-name">
+                      <div class="progress">    
+                        <div class="progress-bar progress-bar-warning" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%">
+                          <span>7.2 MB of 50.0 GB used</span>
+                        </div>
+                      </div>
+                    </div>
+
+                  </div>
+                </div>                                                                        
+              </li>
+            </ul>
+          </div>
+          <!-- END PAGE SIDEBAR -->
 
 
